08f7c1cdf2d73a11b92ed5414e0ab7319b6bc4fc
Kouich Sida <e185705@ie.u-ryukyu.ac.jp>
2019-01-17 14:31:27 +0900

    2nd commit

--- src/main/java/jp/ac/uryukyu/ie/e185705/Main.java
+++ src/main/java/jp/ac/uryukyu/ie/e185705/Main.java
@@ -2,6 +2,11 @@ package jp.ac.uryukyu.ie.e185705;
 
 public class Main {
     public static void main(String[] args){
+        System.out.println("あっち向いてホイをしよう！");
+        System.out.println("せーのっ！　あっち向いてー");
+        Attack attack = new Attack();
+        
     }
+
 
 }
